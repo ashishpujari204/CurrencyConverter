e4604c1770f2bf7e08ee7b7993017f107ace499e
Ashish Agatrao Pujari <ashishagatraopujari@Ashishs-MacBook-Air.local>
2020-05-11 18:38:58 +0530

    implemented encrypted shared perference using andorid jetpack security

--- app/src/main/java/com/ashish/currencyconverter/storage/SharedPreferencesStorage.kt
+++ app/src/main/java/com/ashish/currencyconverter/storage/SharedPreferencesStorage.kt
@@ -1,13 +1,22 @@
 package com.ashish.currencyconverter.storage
 
-
 import android.content.Context
+import android.content.SharedPreferences
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKeys
 import com.ashish.currencyconverter.util.Constants
+
 
 class SharedPreferencesStorage(context: Context) : Storage {
 
+    var masterKeyAlias: String = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC)
 
-    private val sharedPreferences = context.getSharedPreferences(Constants.PREFERANCE_NAME, Context.MODE_PRIVATE)
+    private var sharedPreferences: SharedPreferences =
+        EncryptedSharedPreferences.create(Constants.PREFERANCE_NAME,
+            masterKeyAlias,
+            context,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM)
 
     override fun setString(key: String, value: String) {
         with(sharedPreferences.edit()) {
